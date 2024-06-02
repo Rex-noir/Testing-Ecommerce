ddd22162f3de8fc265916f2869543ddfa2df4172
Lala <ace.rexnoir@gmail.com>
2024-06-02 10:28:48 +0530

    Add image for products component

---
+++ src/components/Slide/Image.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const prop = defineProps(["image"]);
+</script>
+<template>
+  <div
+    :style="{ backgroundImage: `url(${prop.image[0]})` }"
+    class="bg-cover bg-center shadow-lg hover:shadow-md hover:shadow-slate-950"
+  />
+</template>
