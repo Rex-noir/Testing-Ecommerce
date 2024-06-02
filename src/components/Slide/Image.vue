c296ecd0dbcce3167a165169cba510f77046ea06
Lala <ace.rexnoir@gmail.com>
2024-06-02 11:24:44 +0530

    Delete Image

--- src/components/Slide/Image.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-const prop = defineProps(["image"]);
-</script>
-<template>
-  <div
-    :style="{ backgroundImage: `url(${prop.image[0]})` }"
-    class="bg-cover bg-center shadow-lg hover:shadow-md hover:shadow-slate-950"
-  />
-</template>
