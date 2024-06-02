cb69111ecc9bce86a8eea7e43c630e6eee86196a
Lala <ace.rexnoir@gmail.com>
2024-06-02 10:27:34 +0530

    Add basic loading component

---
+++ src/components/Loading/Loading.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+<template>
+  <div
+    id="container"
+    class="col-span-4 mx-auto flex w-full items-center justify-center"
+  >
+    <div
+      id="spinner"
+      class="h-10 w-10 animate-spinner rounded-full border-2 border-solid border-b-red-800 shadow-sm shadow-slate-800 ring-1 ring-red-900"
+    ></div>
+  </div>
+</template>
