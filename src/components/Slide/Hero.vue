b6ec3524bf984704a5985a8e9e3aa92d944e8001
Lala <ace.rexnoir@gmail.com>
2024-06-02 11:24:31 +0530

    Change name to Hero

---
+++ src/components/Slide/Hero.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <div
+      class="mx-auto flex h-[300px] w-full justify-center md:h-[550px] md:w-5/6"
+    >
+      <picture>
+        <source
+          srcset="../../assets/hero_image_640.jpg"
+          media="(max-width: 640px)"
+        />
+        <source
+          srcset="../../assets/hero_image_1920.jpg"
+          media="(max-width: 1920px)"
+        />
+        <img
+          src="../../assets/hero_image_2400.jpg"
+          alt="Image description"
+          class="max-h-full w-full rounded-lg"
+        />
+      </picture>
+    </div>
+  </div>
+</template>
