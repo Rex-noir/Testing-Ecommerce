bf1ecdb6a564cbcb827e00217ac8e93273544d64
Lala <ace.rexnoir@gmail.com>
2024-06-02 10:26:33 +0530

    Added basic filter component

---
+++ src/components/Items/Filter.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import FetchData from "../../utils/fetchData";
+import Loading from "../Loading/Loading.vue";
+
+const data = await FetchData.getCategories();
+const categories = ref(data.json);
+const model = defineModel();
+
+const emits = defineEmits(["updateList"]);
+//selected
+const selected = ref<null | number>(null);
+const isLoading = ref(false);
+
+const markClicked = async (id: number) => {
+  isLoading.value = true;
+  if (selected.value === id) {
+    selected.value = null;
+    model.value = await FetchData.getProducts();
+  } else {
+    selected.value = id;
+    model.value = await FetchData.getProdsByCategoriy(id);
+  }
+  isLoading.value = false;
+};
+</script>
+<template>
+  <div
+    id="filter"
+    class="relative mx-auto flex w-full flex-col justify-center rounded-lg p-2"
+  >
+    <div
+      id="categories"
+      class="mx-auto grid auto-cols-fr grid-cols-2 gap-2 md:grid-cols-4"
+    >
+      <button
+        v-for="category in categories"
+        :key="category.id"
+        :class="[
+          ' overflow-hidden rounded-sm bg-slate-100 p-2 shadow-lg ring-1 ring-slate-500  transition-all duration-300 hover:bg-slate-900 hover:text-slate-50 hover:shadow-slate-800 ',
+          { 'bg-slate-900 text-slate-200': selected === category.id },
+        ]"
+        @click="markClicked(category.id)"
+      >
+        {{ category.name }}
+      </button>
+    </div>
+    <Loading v-if="isLoading" class="absolute translate-y-1/2"></Loading>
+  </div>
+</template>
