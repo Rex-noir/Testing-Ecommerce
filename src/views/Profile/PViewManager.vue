26e9991d1c0e8f0e48d031f1711fa1f3bff3b24b
Lala <ace.rexnoir@gmail.com>
2024-06-03 16:44:41 +0530

    Add template for custom profile

--- src/views/Profile/PViewManager.vue
+++ src/views/Profile/PViewManager.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import { useRoute } from "vue-router";
 import Me from "./Me.vue";
+import ViewProfile from "./ViewProfile.vue";
+
 const route = useRoute();
 </script>
 <template>
   <template v-if="route.params.id === 'me'"><Me></Me></template>
+  <template v-else><ViewProfile></ViewProfile></template>
 </template>
