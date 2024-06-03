a36a1778c2f7bc64e1ed2076bc362dd415908515
Lala <ace.rexnoir@gmail.com>
2024-06-03 11:08:19 +0530

    Add user searching feature

--- src/components/Search/Search.vue
+++ src/components/Search/Search.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import FetchData from "../../utils/fetchData";
-import { Product } from "../../interfaces";
+import { Product, User } from "../../interfaces";
 import Loading from "../Loading/Loading.vue";
 
-const show = ref<boolean>(false);
+const showProducts = ref<boolean>(false);
+const showUsers = ref<boolean>(false);
 const data = ref<null | Product[]>(null);
-const result = ref<Product[] | null>(null);
+const productResults = ref<Product[] | null>(null);
+const userResults = ref<User[] | null>(null);
 const loading = ref<boolean>(false);
 
 const notFound = ref<boolean>(false);
@@ -17,27 +19,16 @@ const updateShow = (event: Event) => {
   if (input.value.length > 0) {
     search(input.value);
   } else {
-    show.value = false;
+    showProducts.value = false;
   }
 };
+const isProducts = ref<boolean>(true);
 
 const search = async (query: string) => {
   try {
-    loading.value = true;
-    isError.value = false;
-
-    const response = await FetchData.getProducts(undefined, { title: query });
-    data.value = (await response.json) as Product[];
-
-    if (data.value && data.value.length > 0) {
-      result.value = data.value;
-      loading.value = false;
-      show.value = true;
-    } else {
-      result.value = null;
-      show.value = false;
-      notFound.value = true;
-      loading.value = false;
+    if (isProducts.value === true) searchProducts(query);
+    else {
+      searchUsers(query);
     }
   } catch (error) {
     notFound.value = true;
@@ -45,10 +36,52 @@ const search = async (query: string) => {
     loading.value = false;
   }
 };
+
+const searchUsers = async (query: string) => {
+  loading.value = true;
+  const users = await FetchData.getAllUsers();
+  const filtered = users.filter((user) =>
+    user.name.toLowerCase().includes(query.toLowerCase()),
+  );
+  showProducts.value = false;
+  loading.value = false;
+
+  if (filtered.length > 0) {
+    userResults.value = filtered;
+    showUsers.value = true;
+  } else {
+    userResults.value = null;
+    showUsers.value = false;
+    notFound.value = true;
+  }
+};
+
+const searchProducts = async (query: string) => {
+  loading.value = true;
+  isError.value = false;
+  showUsers.value = false;
+
+  const response = await FetchData.getProducts(undefined, { title: query });
+  data.value = (await response.json) as Product[];
+
+  if (data.value && data.value.length > 0) {
+    productResults.value = data.value;
+    loading.value = false;
+    showProducts.value = true;
+  } else {
+    productResults.value = null;
+    showProducts.value = false;
+    notFound.value = true;
+    loading.value = false;
+  }
+};
 </script>
 <template>
   <div class="relative mx-auto w-full">
-    <div v-if="show" class="fixed inset-0 z-10 bg-black opacity-50"></div>
+    <div
+      v-if="showProducts || showUsers"
+      class="fixed inset-0 z-10 bg-black opacity-50"
+    ></div>
     <div class="relative z-20 flex items-center justify-center">
       <input
         type="search"
@@ -57,27 +90,46 @@ const search = async (query: string) => {
         placeholder="Search"
         @input="updateShow"
         @focusout="
-          show = false;
+          showProducts = false;
           notFound = false;
+          showUsers = false;
         "
         class="w-full rounded-md bg-slate-200 p-2 shadow-sm focus-within:outline-none"
       />
+      <button
+        @click="isProducts = !isProducts"
+        :class="{
+          ['bg-slate-900 text-white']: !isProducts,
+          'bg-none': isProducts,
+        }"
+        class="- absolute right-8 rounded-lg border-2 border-stone-900 px-2 transition-all duration-200 hover:scale-90 active:scale-105"
+      >
+        Users
+      </button>
     </div>
     <Loading v-if="loading" class="absolute z-50 mt-3"></Loading>
     <div v-if="notFound" class="absolute z-50 mt-3 w-full text-center">
-      No result!
+      No productResults!
     </div>
     <div
-      id="result-container"
-      v-show="show"
-      class="absolute z-30 row-start-2 mt-2 flex w-full flex-col gap-2 overflow-y-auto rounded-md bg-slate-900 p-2 text-slate-50 shadow-lg shadow-slate-900 backdrop-brightness-50"
+      id="productResults-container"
+      v-show="showProducts || showUsers"
+      class="absolute z-30 row-start-2 mt-2 flex w-full flex-col gap-2 overflow-y-auto rounded-md bg-slate-900 p-2 text-slate-50 shadow-sm backdrop-brightness-50"
     >
       <div>
         <button
-          v-for="product in result"
-          class="w-full rounded-lg p-2 text-left hover:bg-slate-50 hover:text-black"
+          v-for="product in productResults"
+          v-if="showProducts"
+          class="w-full rounded-sm p-2 text-left hover:bg-slate-50 hover:text-black"
         >
           {{ product.title }}
+        </button>
+        <button
+          v-for="product in userResults"
+          v-if="showUsers"
+          class="w-full rounded-sm p-2 text-left hover:bg-slate-50 hover:text-black"
+        >
+          {{ product.name }}
         </button>
       </div>
     </div>
@@ -90,12 +142,12 @@ input#search {
   background-position: 10px 10px;
   padding-left: 40px;
 }
-#result-container::-webkit-scrollbar {
+#productResults-container::-webkit-scrollbar {
   display: none;
 }
 
 /* Hide scrollbar for IE, Edge and Firefox */
-#result-container {
+#productResults-container {
   -ms-overflow-style: none; /* IE and Edge */
   scrollbar-width: none; /* Firefox */
 }
