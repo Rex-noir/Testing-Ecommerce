83887a48595c54b6f2ea616ddde2768ed10f7fd5
Lala <ace.rexnoir@gmail.com>
2024-06-03 17:14:47 +0530

    Add path for individual products

--- src/router/router.ts
+++ src/router/router.ts
@@ -4,6 +4,7 @@ import About from "../views/About.vue";
 import Home from "../views/Home.vue";
 import Authentication from "../views/Auth/Authentication.vue";
 import PViewManager from "../views/Profile/PViewManager.vue";
+import ProductView from "../views/Products/ProductView.vue";
 interface Route {
   path: string;
   name: string;
@@ -14,6 +15,7 @@ const routes: Array<Route> = [
   { path: "/", component: Home, name: "Home" },
   { path: "/auth/:type", component: Authentication, name: "Authentication" },
   { path: "/profile/:id", component: PViewManager, name: "Profile" },
+  { path: "/products/:id", component: ProductView, name: "Product" },
 ];
 const router = createRouter({ history: createMemoryHistory(), routes });
 export default router;
