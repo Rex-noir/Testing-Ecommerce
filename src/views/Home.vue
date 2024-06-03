4eef526078264ede1150b7872e59250a881a3822
Lala <ace.rexnoir@gmail.com>
2024-06-03 09:39:33 +0530

    Make fallback text inside the items div

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,22 +10,26 @@ let data = ref(await FetchData.getProducts());
 const items = computed(() => data.value);
 </script>
 <template>
-  <div class="grid auto-rows-auto gap-3">
+  <div class="grid auto-rows-auto gap-6">
     <div id="carousel" class="row-start-1 w-full">
       <Hero class="my-auto"></Hero>
     </div>
     <div id="container" class="row-start-2 flex flex-col gap-3 rounded-lg">
+      <p class="text-center font-bold">Categories</p>
       <Filter v-model="data"></Filter>
       <div
         id="list-container"
-        class="grid auto-rows-fr grid-cols-[repeat(auto-fill,_300px)] place-content-center gap-5 rounded-lg bg-gray-200 py-10 shadow-lg"
+        class="relative grid auto-rows-fr grid-cols-[repeat(auto-fill,_300px)] place-content-center items-center gap-5 rounded-lg bg-gray-200 py-10 shadow-lg"
         v-if="items"
       >
         <Item v-for="item in items.json" :data="item" :key="item.id"></Item>
+        <p
+          v-show="items.json.length <= 0"
+          class="absolute w-full text-center text-slate-500"
+        >
+          Empty list!
+        </p>
       </div>
     </div>
-    <p v-show="items.json.length <= 0" class="text-center text-slate-500">
-      Empty list!
-    </p>
   </div>
 </template>
