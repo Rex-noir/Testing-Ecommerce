dc4775204b2e5fbd0bb4a1a642207a3fcb4ec33d
Lala <ace.rexnoir@gmail.com>
2024-06-03 12:14:16 +0530

    Change the hero images layout design

--- src/components/Slide/Hero.vue
+++ src/components/Slide/Hero.vue
@@ -1,4 +1,9 @@
 <script setup lang="ts">
+import { onMounted, ref } from "vue";
+import savingIMG from "../../assets/undraw_savings_re_eq4w.svg";
+import transferIMG from "../../assets/undraw_transfer_money_re_6o1h.svg";
+import shopoingIMG from "../../assets/undraw_web_shopping_re_owap.svg";
+
 const slideUP = () => {
   const targetElement = document.querySelector("#list-container");
 
@@ -8,9 +13,22 @@ const slideUP = () => {
     window.scroll({ top: 0, behavior: "smooth" });
   }
 };
+
+const images: { src: string; text: string }[] = [
+  { src: savingIMG, text: "Buy more and save more!" },
+  { src: transferIMG, text: "No need to worry about payment method!" },
+  { src: shopoingIMG, text: "Shop directly! No signup required!" },
+];
+const currentIndex = ref<number>(0);
+
+onMounted(() => {
+  setInterval(() => {
+    currentIndex.value = (currentIndex.value + 1) % images.length;
+  }, 2000);
+});
 </script>
 <template>
-  <div class="grid w-full gap-5 md:grid-cols-2">
+  <div class="grid w-full grid-rows-2 gap-5 md:grid-cols-2 md:grid-rows-1">
     <div>
       <div
         class="row-start-1 my-auto flex h-full flex-col justify-center gap-6 rounded-lg p-1 md:p-5"
@@ -41,20 +59,20 @@ const slideUP = () => {
       </div>
     </div>
     <div
-      class="row-start-2 hidden rounded-lg shadow-lg shadow-slate-300 hover:shadow-slate-400 md:col-start-2 md:row-start-1 md:grid"
+      class="relative row-start-2 mx-auto h-[350px] w-3/4 rounded-lg p-1 text-center shadow-lg shadow-slate-300 hover:shadow-slate-400 md:col-start-2 md:row-start-1"
     >
-      <div
-        class="relative col-start-2 row-span-2 row-start-1 h-full p-3 text-center"
-      >
+      <Transition enter-active-class="scale-0" leave-active-class="scale-0">
         <img
-          src="../../assets/undraw_transfer_money_re_6o1h.svg"
-          class="mx-auto h-full w-5/6 p-5"
+          :src="images[currentIndex].src"
+          :key="currentIndex"
+          class="duration-2000 absolute size-80 w-full p-10 transition-all ease-in-out"
           alt="hero image"
         />
-        <span class="absolute bottom-3 left-0 w-full"
-          >No need to worry about paymet methods!</span
-        >
-      </div>
+      </Transition>
+      <span
+        class="duration-2000 absolute bottom-3 left-0 w-full transition-all"
+        >{{ images[currentIndex].text }}</span
+      >
     </div>
   </div>
 </template>
