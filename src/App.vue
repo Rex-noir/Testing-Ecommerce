f900419baf6279982401fe04ec062683b8aa834c
Lala <ace.rexnoir@gmail.com>
2024-06-04 12:56:52 +0530

    Load userInfo before MOUNT

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <script setup lang="ts">
-import { onBeforeMount } from "vue";
+import { onBeforeMount, ref } from "vue";
 import Header from "./components/Header/Header.vue";
 import Loading from "./components/Loading/Loading.vue";
 import { useUserInfo } from "./stores/user";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
+const userStore = useUserInfo();
+const loaded = ref<boolean>(false);
 
 onBeforeMount(async () => {
-  const user = useUserInfo();
-  await user.loadUser();
+  try {
+    await userStore.loadUser();
+  } finally {
+    loaded.value = true;
+  }
 });
 </script>
 
 <template>
   <header
+    v-if="loaded"
     class="row-start-1 row-end-1 m-2 h-fit rounded-lg shadow-lg shadow-slate-300"
   >
     <Header></Header>
