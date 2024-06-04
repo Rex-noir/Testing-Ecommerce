36e67a9baeaef8d689165e8f5b839032ae426a2c
Lala <ace.rexnoir@gmail.com>
2024-06-04 12:18:43 +0530

    Add new route for Sell.vue

--- src/router/router.ts
+++ src/router/router.ts
@@ -5,6 +5,7 @@ import Home from "../views/Home.vue";
 import Authentication from "../views/Auth/Authentication.vue";
 import PViewManager from "../views/Profile/PViewManager.vue";
 import ProductView from "../views/Products/ProductView.vue";
+import Sell from "../views/Sell/Sell.vue";
 interface Route {
   path: string;
   name: string;
@@ -16,6 +17,7 @@ const routes: Array<Route> = [
   { path: "/auth/:type", component: Authentication, name: "Authentication" },
   { path: "/profile/:id", component: PViewManager, name: "Profile" },
   { path: "/products/:id", component: ProductView, name: "Product" },
+  { path: "/products/sell", component: Sell, name: "Sell" },
 ];
 const router = createRouter({ history: createWebHistory(), routes });
 export default router;
