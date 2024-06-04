5631540038413d7d76284fd13d5875e24a9dbf94
Lala <ace.rexnoir@gmail.com>
2024-06-04 12:57:03 +0530

    Remove top level await

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,9 @@ import "./style.css";
 import App from "./App.vue";
 import router from "./router/router";
 import { createPinia } from "pinia";
-import { useUserInfo } from "./stores/user";
 
 const pinia = createPinia();
 const app = createApp(App);
 app.use(router);
 app.use(pinia);
-await useUserInfo()
-  .loadUser()
-  .then(() => {
-    app.mount("#app");
-  });
+app.mount("#app");
